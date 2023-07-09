929edd7a068dbaa4b1871ed6687333aaed975c10
Pavel Lukashik <pavel_lukashik@epam.com>
2023-07-09 22:35:09 +0400

    test commit

--- aws_variables.tf
+++ aws_variables.tf
@@ -3,13 +3,13 @@ variable "aws_region" {
     default = "eu-central-1"  
 }
 
-variable "aws_access_key" {
-    type = string
-}
+# variable "aws_access_key" {
+#     type = string
+# }
 
-variable "aws_secret_key" {
-    type = string
-}
+# variable "aws_secret_key" {
+#     type = string
+# }
 
 variable "ecs_name" {
     type = string
--- manifest.tf
+++ manifest.tf
@@ -1,7 +1,7 @@
 module "dev-webserver" {
   source = "./AWS"
   aws_region = var.aws_region
-  aws_access_key = var.aws_access_key
-  aws_secret_key = var.aws_secret_key
+  # aws_access_key = var.aws_access_key
+  # aws_secret_key = var.aws_secret_key
   ecs_name = var.ecs_name
 }
