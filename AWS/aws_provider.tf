fc8debeb304f0dd958f9d12b56ed1b0886139a30
Pavel Lukashik <pavel_lukashik@epam.com>
2023-07-09 19:18:38 +0400

    variables vere moved to separate file

--- AWS/aws_provider.tf
+++ AWS/aws_provider.tf
@@ -1,16 +1,3 @@
-variable "aws_region" {
-    type = string
-    default = "eu-central-1"  
-}
-
-variable "aws_access_key" {
-    type = string
-}
-
-variable "aws_secret_key" {
-    type = string
-}
-
 provider "aws" {
     region = var.aws_region
     access_key = var.aws_access_key
--- AWS/aws_variables.tf
+++ AWS/aws_variables.tf
@@ -1,3 +1,16 @@
+variable "aws_region" {
+    type = string
+    default = "eu-central-1"  
+}
+
+variable "aws_access_key" {
+    type = string
+}
+
+variable "aws_secret_key" {
+    type = string
+}
+
 variable "ecs_name" {
     type = string
     default = "test_ecs_repo"  
