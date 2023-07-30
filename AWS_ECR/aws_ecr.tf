d8923043bb7ec9af230fcb0faa17481a59179790
Pavel Lukashik <lykshik@gmail.com>
2023-07-30 14:18:29 +0400

    aws ecr was reconfigured

--- AWS_ECR/aws_ecr.tf
+++ AWS_ECR/aws_ecr.tf
@@ -3,6 +3,7 @@ resource "aws_ecr_repository" "ecr_reg" {
   image_scanning_configuration {
 	    scan_on_push = true
 	  }
+  force_destroy = true
 }
 
 resource "aws_ecr_repository_policy" "demo-repo-policy" {
