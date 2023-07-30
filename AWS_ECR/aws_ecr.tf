69f510527c3cf22eab08149b931cd806e2dc8446
Pavel Lukashik <lykshik@gmail.com>
2023-07-30 13:04:41 +0400

    aws ecr was reconfigured

--- AWS_ECR/aws_ecr.tf
+++ AWS_ECR/aws_ecr.tf
@@ -1,9 +1,31 @@
 resource "aws_ecr_repository" "ecr_reg" {
-#  name = "${var.ecr_name}-${var.aws_region}"
-  name = "${var.docker_image_name}-${var.docker_image_author}"
+  name = "${var.ecr_name}-${var.aws_region}"
+#  name = "${var.docker_image_name}-${var.docker_image_author}"
   image_scanning_configuration {
 	    scan_on_push = true
 	  }
+}
+
+resource "aws_ecr_lifecycle_policy" "default_policy" {
+  repository = aws_ecr_repository.ecr_reg.name
+	  policy = <<EOF
+	{
+	    "rules": [
+	        {
+	            "rulePriority": 1,
+	            "description": "Keep only the last ${var.ecr_untagged_images_count} untagged images.",
+	            "selection": {
+	                "tagStatus": "untagged",
+	                "countType": "imageCountMoreThan",
+	                "countNumber": ${var.ecr_untagged_images_count}
+	            },
+	            "action": {
+	                "type": "expire"
+	            }
+	        }
+	    ]
+	}
+	EOF
 }
 
 data "aws_ecr_authorization_token" "token" {}
--- variables_aws.tf
+++ variables_aws.tf
@@ -8,6 +8,11 @@ variable "ecr_name" {
     default = "test_ecr_reg"  
 }
 
+variable "ecr_untagged_images_count" {
+    type = string
+    default = "3"  
+}
+
 variable "ecs_cluster_name" {
     type = string
     default = "test_ecs_cluster"
