f665f40f1b85a90cc090236254b407b0b30e8c8b
Pavel Lukashik <lykshik@gmail.com>
2023-07-30 13:39:51 +0400

    docker image was reconfigured

--- docker/docker_image.tf
+++ docker/docker_image.tf
@@ -15,7 +15,7 @@ resource "docker_image" "test_docker_image" {
   name = "${var.ecr_url}:latest"
   build {
     context = "./repo/"
-#    tag     = [ "${var.ecr_url}:latest", "${var.docker_image_tag}"]
+    tag     = [ "${var.ecr_url}:latest", "${var.docker_image_tag}"]
     label = {
       author : "${var.docker_image_author}"
     }
