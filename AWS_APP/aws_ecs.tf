63bdc880238b4fdb299a2be24556f2f8d21fc84b
Pavel Lukashik <lykshik@gmail.com>
2023-08-10 21:50:57 +0400

    aws iam role for ecs was added

--- AWS_APP/aws_ecs.tf
+++ AWS_APP/aws_ecs.tf
@@ -2,11 +2,34 @@ resource "aws_ecs_cluster" "test-ecs-cluster" {
   name = "${var.ecs_cluster_name}-${var.aws_region}"
 }
 
-resource "aws_ecs_service" "test-ecs-service-two" {
+resource "aws_ecs_service" "test-ecs-service" {
   name = "${var.ecs_service_name}-${var.aws_region}-app"
   cluster = aws_ecs_cluster.test-ecs-cluster.id
   task_definition = aws_ecs_task_definition.test-ecs-task-definition.arn
   desired_count = 1
+}
+
+resource "aws_iam_role" "test-iam-role" {
+  name = "${var.ecs_service_name}-${var.aws_region}-iam"
+  assume_role_policy = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Action": "sts:AssumeRole",
+            "Principal": {
+            "Service": [
+                "s3.amazonaws.com",
+                "lambda.amazonaws.com",
+                "ecs.amazonaws.com"
+            ]
+            },
+            "Effect": "Allow",
+            "Sid": ""
+        }
+    ]
+}
+EOF
 }
 
 resource "aws_ecs_task_definition" "test-ecs-task-definition" {
@@ -15,11 +38,14 @@ resource "aws_ecs_task_definition" "test-ecs-task-definition" {
   network_mode             = "awsvpc"
   memory             = 1024
   cpu                = 512
+  task_role_arn            = "${aws_iam_role.test-iam-role.arn}"
+  execution_role_arn       = "${data.aws_iam_role.ecs_task_execution_role.arn}"
   container_definitions    = <<EOF
 [
   {
     "name": "${var.ecs_service_name}-${var.aws_region}-app",
     "image": "${var.ecr_url}:latest",
+    "networkMode": "awsvpc",
     "portMappings": [
       {
         "containerPort": ${var.task_port},
